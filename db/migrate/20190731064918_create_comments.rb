9fc0f97f46d0c4b02fe031a57650537e6276f2ae
yamatatu00 <tattuxan@swan.ocn.ne.jp>
2019-09-01 14:16:58 +0900

    edit-README&model

--- db/migrate/20190731064918_create_comments.rb
+++ db/migrate/20190731064918_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.integer :user_id
-      t.integer :product_id
-      t.text    :comment
+      t.text       :comment  , null: false
+      t.references :user     , foreign_key: true
+      t.references :product  , foreign_key: true
       t.timestamps
     end
   end

bbd7e4f498590756e0617fda95ccdddc4bf79782
yamatatu00 <tattuxan@swan.ocn.ne.jp>
2019-10-28 16:34:09 +0900

    test-users-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  with_options presence: true do
-    validates :name
-    validates :email
-    validates_uniqueness_of :name
-    validates_uniqueness_of :email
-  end
+
   has_many :products, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :like_stories, through: :likes, source: :product
+
+  validates :name , presence: true, uniqueness: true, length: { maximum: 6 } 
+  validates :email, presence: true, uniqueness: true
+  
 end
